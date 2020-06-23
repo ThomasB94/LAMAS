c8086108d83fae3d30406ffa66551fdcd8a5a421
ThomasB94 <thomas__bakker@hotmail.com>
2020-06-23 22:47:13 +0200

    updates

--- website/routing-app/src/app/first/first.component.ts
+++ website/routing-app/src/app/first/first.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-first',
@@ -8,8 +8,27 @@ import { Component, OnInit } from '@angular/core';
 export class FirstComponent implements OnInit {
 
   constructor() { }
-
+  mybutton = null;
   ngOnInit(): void {
+    this.mybutton = document.getElementById("myBtn");
+    console.log("my button is: " + this.mybutton);
+  }
+
+  scrollFunction(): void {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      this.mybutton.style.display = "block";
+    } else {
+      this.mybutton.style.display = "none";
+    }
+  }
+  topFunction(): void {
+    console.log('executed');
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
+  @HostListener("window:scroll", []) onWindowScroll() {
+    // do some stuff here when the window is scrolled
+    this.scrollFunction();
   }
 
 }
