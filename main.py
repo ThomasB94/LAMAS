6b97942392eb0e15a9549b89100be2133a288ea3
Daan <daan.lambert@gmail.com>
2020-06-29 16:16:02 +0200

    testing

--- main.py
+++ main.py
@@ -6,20 +6,21 @@ def main():
     won_counter = 0
     removed = 0
     announcements = 0
+    num_of_games = 50
     t = time.time()
-    for _ in range(3):
+    for _ in range(num_of_games):
         game = Game(2, 10, 'range')
         if game.won:
             won_counter = won_counter + 1
         removed = removed + game.removed_worlds
         announcements = announcements + game.announcements_made
     print("+++++++++++++++++++++++++++++++++++++++")
-    print("Eventually won", won_counter/3, "of matches")
+    print("Eventually won", won_counter/num_of_games, "of matches")
     print(won_counter)
     if announcements == 0:
         print("No announcements were done")
     else:
-        print("On average ", (removed / announcements) / 3, " worlds removed per announcement")
+        print("On average ", (removed / announcements) / num_of_games, " worlds removed per announcement")
     elapsed = time.time() - t
     print("This took", elapsed, "seconds")
 
