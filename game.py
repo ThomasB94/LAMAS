57c8b5751e1e291252564be91064252d4cf6ed80
Paul Hofman <paulwhofman@hotmail.com>
2020-06-29 14:40:04 +0200

    Working version, with some debug stuff

--- game.py
+++ game.py
@@ -15,6 +15,7 @@ class Game():
         self.num_initial_cards = 2
         self.won = False
         self.announcement_type = announcements
+        print("============================================")
         print("Setting up game with", num_agents, "agents and", top_card, "as the highest cards")
         print(announcements, "is the announcements setting")
         print("--------------------------------------------")
@@ -44,14 +45,14 @@ class Game():
             self.agents.append(Agent(agent_idx, hand, self))
             print("Agent", agent_idx + 1, "has cards", hand)
         print("Remaing cards are:", self.remaining)
-        print("-------------------------")
 
     # A round is a round of announcements after which an agent decides which stack to put their card on
     def game_loop(self):
         agent_turn = 0
         round = 1
         while True:
-            print("----------------------------------")
+            print("SELF REAMINING", self.remaining)
+            print("--------------------------------------------")
             print("Starting round", round)
             # print("Every agent will make an announcement, after which")
             # print("agent", agent_turn + 1, "will decide which table stack to put a card on")
@@ -70,20 +71,20 @@ class Game():
             agent = self.agents[agent_turn]
 
             if not agent.can_make_move():
-                print("Agent", agent_turn + 1, "can't make a move, so the game is lost")
-                print("HERE AGENT STACKS", self.agents[0].hand, self.agents[1].hand)
+                print("--Agent", agent_turn + 1, "can't make a move, so the game is lost")
+                print("Current agent had: ", self.agents[0].hand, "other agent had:", self.agents[1].hand)
                 break
 
 
             # print("Agent had stack", self.agents[agent_turn].hand)
             print("Current agent stack:", self.agents[agent_turn].hand, "other agent stack:", self.agents[agent_turn-1].hand)
             card, stack_idx = agent.make_move()
-            print("agent put", card, "on stack", stack_idx)
+            print("Current agent put", card, "on stack", stack_idx)
 
             agent.take_card()
 
             if self.game_won():
-                print("Game is won, because all agents have 0 cards left")
+                print("++Game is won, because all agents have 0 cards left")
                 self.won = True
                 break
             agent_turn = agent_turn + 1
@@ -91,9 +92,11 @@ class Game():
                 agent_turn = 0
             round = round + 1
 
-
+    # the game is won if there are no cards left on the stack and both players have no cards left
     def game_won(self):
         status = True
+        # if self.remaining is not []:
+        #     status = False
         for agent in self.agents:
             if agent.hand:
                 status = False
